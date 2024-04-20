7f9ec7638a2aea31664c9143a44aa742533e9239
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-04-19 21:09:34 -0700

    update libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,29 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
+#define LED_PIN     6
+#define NUM_LEDS    128
 
-#define LED_PIN 6 // 将数据线连接到Arduino的6号引脚
-#define LED_COUNT 64 * 4 // WS2812 8x8共有64个LED
-
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  // Serial.begin(9600);
-  strip.begin();
-  strip.show(); // 初始化所有LED为关闭状态
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(60);
+    FastLED.clear();
 }
 
 void loop() {
-  for(int i=0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(255, 255, 255)); // 设置当前LED为白色
-    strip.show(); // 显示当前LED的颜色
-    delay(50); // 延迟一段时间，让LED保持亮起状态
-    strip.setPixelColor(i, strip.Color(0, 0, 0)); // 关闭当前LED
-    strip.show(); // 显示关闭当前LED
-    delay(50); // 延迟一段时间，让LED保持熄灭状态
-  }
-}
+    // Turn on all LEDs to red
 
+    // fill_solid(leds, NUM_LEDS, CRGB::Red);
+    // FastLED.show();
+    // delay(1000);
+
+    // // Turn off all LEDs
+    // fill_solid(leds, NUM_LEDS, CRGB::Black);
+    // FastLED.show();
+    // delay(1000);
+
+    leds[1].setHSV(0,255,255);
+    FastLED.show();
+}
