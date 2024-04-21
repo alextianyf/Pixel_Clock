15fdfdc55847a68c3d552860e4c43b0999f28513
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-04-20 17:48:14 -0700

    number display successful

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
 #include <FastLED.h>
 #include <FastLED_NeoMatrix.h>
-#include <my_font.h>
 
-#define MATRIXWIDTH         32
+#define MATRIXWIDTH         8
 #define MATRIXHEIGHT        8
 
 #define LED_PIN             6
@@ -14,31 +13,31 @@
 #define COLOR_ORDER         GRB
 
 CRGB leds[NUM_LEDS];
-FastLED_NeoMatrix *matrix;
+
+const uint32_t ledarray0[] PROGMEM = {
+    //    padding bits   R   G  B
+    // 0x       00       00  00 00
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+    0x00000000, 0x00000000, 0x000000FF, 0x00000000, 0x00000000, 0x000000FF, 0x00000000, 0x00000000, 
+    0x00000000, 0x00000000, 0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF, 0x00000000, 
+    0x00000000, 0x00000000, 0x000000FF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+};
 
 void setup() {
     Serial.begin(115200);
     FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-    matrix = new FastLED_NeoMatrix(leds,MATRIXWIDTH,MATRIXHEIGHT,NEO_MATRIX_BOTTOM + NEO_MATRIX_LEFT + NEO_MATRIX_COLUMNS);
-    matrix->setTextWrap(false);
-    matrix->setBrightness(50);
-    matrix->setFont(&Picopixel);
-    matrix->clear();
-    matrix->setBrightness(BRIGHTNESS);
-}
-
-void ShowMessage(char *message, int len, uint32_t color)
-{
-    matrix->clear();
-    matrix->setTextColor(color);
-    if (len < 1 || len > 8)
-        return;
-    matrix->setCursor(1 + (8 - len) * 2, 5); //设置鼠标光标的位置用以确定在哪里显示文字
-
-    matrix->print(message);
-    matrix->show();
+    FastLED.setBrightness(100);
+    FastLED.clear();
 }
 
 void loop() {
-    ShowMessage("BOOT", 4, matrix->Color(255, 0, 255) );
+    for(int i = 0; i<NUM_LEDS; i++){
+        leds[i] = pgm_read_dword(&(ledarray0[i]));
+    }
+    FastLED.show();
 }
+
